4dc4f58af55f839c92fc3b6f0dffe31ef280989c
Andreas Bauer <andi.bauer@fau.de>
2020-02-27 09:50:16 +0100

    Add cli argument support for client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/osrgroup/product-model-toolkit/pkg/version"
@@ -9,7 +10,25 @@ import (
 var gitCommit string
 
 func main() {
+	initFlags()
+
 	fmt.Println("Hello, I'm the client.")
+}
+
+func initFlags() {
+	version := flag.Bool("v", false, "show version")
+
+	flag.Parse()
+
+	if *version {
+		printVersion()
+	}
+}
+
+func printVersion() {
+	fmt.Println("PMT Client")
+	fmt.Println("----------")
 	fmt.Println("Version: " + version.Name())
 	fmt.Println("Git commit: " + gitCommit)
+	fmt.Println("----------")
 }
