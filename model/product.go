2a69c57300ac6bae74f19a539af05c4018e48221
Andreas Bauer <andi.bauer@fau.de>
2020-03-05 11:37:56 +0100

    model: Format product.go

--- model/product.go
+++ model/product.go
@@ -1,39 +1,39 @@
 package model
 
-// Product represents a software product and its main properties. 
+// Product represents a software product and its main properties.
 // It is the root element in the product architecture model.
 type Product struct {
-	Name string
-	Description string
-	Version string
-	VCS string
-	ClearingState interface{} // TODO: Specify type
+	Name           string
+	Description    string
+	Version        string
+	VCS            string
+	ClearingState  interface{} // TODO: Specify type
 	Infrastructure Infrastructure
 }
 
 // Component represents a unit of compositon of the product, e.g. class, lib, module.
-type Component struct {}
+type Component struct{}
 
 // Dependency represents the relationship between two components.
-type Dependency struct {}
+type Dependency struct{}
 
 // License represents a open source license.
-type License struct {}
+type License struct{}
 
 // Copyright represents a copyright statement.
-type Copyright struct {}
+type Copyright struct{}
 
 // Policy represents user/company specific rules for the usage of components and its licenses.
-type Policy struct {}
+type Policy struct{}
 
 // Artifact represents a digital artifact like source code files or binaries.
-type Artifact struct {}
+type Artifact struct{}
 
 // Deliverable represents a subset a product's components that are deliverable.
-type Deliverable struct {} // deployment unit
+type Deliverable struct{} // deployment unit
 
 // Infrastructure represents a dependency to required infrastructure, e.g. a runtime enviroment in a specific version.
-type Infrastructure struct {} // Inf. dep. e.g. Java Runtime
+type Infrastructure struct{} // Inf. dep. e.g. Java Runtime
 
 // Vulnerability represents known security vulnerabilities of software components.
-type Vulnerability struct {} // e.g. CVE
+type Vulnerability struct{} // e.g. CVE
