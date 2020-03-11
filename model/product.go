55853f837b3980acd8c150497d7d0302677a1942
Andreas Bauer <andi.bauer@fau.de>
2020-03-11 16:02:54 +0100

    model: Format product.go

--- model/product.go
+++ model/product.go
@@ -3,17 +3,18 @@ package model
 // Product represents a software product and its main properties.
 // It is the root element in the product architecture model.
 type Product struct {
-	Name           string
-	Description    string
-	Version        string
-	VCS            string
-	ClearingState  interface{} // TODO: Specify type
-	RootComponents []Component
+	Name          string
+	Description   string
+	Version       string
+	VCS           string
+	ClearingState interface{} // TODO: Specify type
+	RootDep       []*Dependency
+	Components    []Component
 	Infrastructure
 }
 
 // License represents a open source license.
-type License struct{
+type License struct {
 	SPDXID string
 }
 
@@ -32,8 +33,8 @@ type Artifact struct {
 // Deliverable represents a subset a product's components that are deliverable.
 type Deliverable struct{} // deployment unit
 
-// Infrastructure represents a dependency to required infrastructure, e.g. a runtime enviroment in a specific version.
-type Infrastructure struct{} // Inf. dep. e.g. Java Runtime
+// Infrastructure represents a dependency to required infrastructure, e.g. a runtime enviroment like the Java Runtime in a specific version.
+type Infrastructure struct{}
 
-// Vulnerability represents known security vulnerabilities of software components.
-type Vulnerability struct{} // e.g. CVE
+// Vulnerability represents known security vulnerabilities of software components, e.g. CVE
+type Vulnerability struct{}
