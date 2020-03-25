ae561db835a0fe57a35d7436c1b08fb626a9e02c
Andreas Bauer <andi.bauer@fau.de>
2020-03-25 21:20:29 +0100

    Add Scancode as new scanner

--- pkg/scanner/scanner.go
+++ pkg/scanner/scanner.go
@@ -9,9 +9,20 @@ type Scanner struct {
 
 // Available list all available scanner tools that can be used.
 var Available = [...]Scanner{
-	Scanner{
-		Name:      "Licensee",
-		Version:   "9.13.0",
-		DockerImg: "docker.pkg.github.com/osrgroup/product-model-toolkit/scanner-licensee:9.13.0",
-	},
+	Licensee,
+	Scancode,
+}
+
+// Licensee represents the latest usable Licensee version
+var Licensee = Scanner{
+	Name:      "Licensee",
+	Version:   "9.13.0",
+	DockerImg: "docker.pkg.github.com/osrgroup/product-model-toolkit/scanner-licensee:9.13.0",
+}
+
+// Scancode represents the latest usable Scancode version
+var Scancode = Scanner{
+	Name:      "Scancode",
+	Version:   "3.1.1",
+	DockerImg: "docker.pkg.github.com/osrgroup/product-model-toolkit/scanner-scancode:3.1.1",
 }
