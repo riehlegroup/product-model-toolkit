383bbd60e5dd8555b0de1478c187a38f45a660d9
Andreas Bauer <andi.bauer@fau.de>
2020-03-26 19:09:39 +0100

    Refactor main to hadle flags better and use scanner

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -6,35 +6,60 @@ import (
 	"os"
 
 	"github.com/osrgroup/product-model-toolkit/pkg/scanner"
+	"github.com/osrgroup/product-model-toolkit/pkg/scanning"
 	"github.com/osrgroup/product-model-toolkit/pkg/version"
 )
 
 var gitCommit string
 
-func main() {
-	usedFlag := initFlags()
-	if usedFlag {
-		os.Exit(0)
-	}
-
-	fmt.Println("Hello, I'm the client.")
+type flags struct {
+	version    *bool
+	lstScanner *bool
+	scanner    string
+	inDir      string
+	set        bool
 }
 
-func initFlags() bool {
+func main() {
+	flg := initFlags()
+	printInfos(&flg)
+
+	scn := scanner.FromStr(flg.scanner)
+	cfg := &scanner.Config{Tool: scn, InDir: flg.inDir, ResultDir: "/tmp/pm/"}
+
+	scanning.Run(cfg)
+
+}
+
+func initFlags() flags {
 	version := flag.Bool("v", false, "show version")
-	list := flag.Bool("l", false, "list all available scanner")
+	lstScanner := flag.Bool("l", false, "list all available scanner")
+	scanner := flag.String("s", "", "scanner to to use from list of available scanner")
+	wd, _ := os.Getwd()
+	inDir := flag.String("i", wd, "input dir to scan. Default is current working directory")
 
 	flag.Parse()
 
-	if *version {
+	set := flag.NFlag() > 0
+
+	return flags{
+		version,
+		lstScanner,
+		*scanner,
+		*inDir,
+		set,
+	}
+}
+
+func printInfos(flg *flags) {
+	if *flg.version {
 		printVersion()
 	}
 
-	if *list {
+	if *flg.lstScanner {
 		listScanner()
 	}
 
-	return flag.NFlag() > 0
 }
 
 func printVersion() {
