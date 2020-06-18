a3e6d5113eb47d64c708cb0868cd3321bfa981a6
Andreas Bauer <dev@andreasbauer.org>
2020-06-18 17:50:17 +0200

    Add converter for PHP Composer documents

---
+++ pkg/convert/composer/convert.go
@@ -0,0 +1,61 @@
+// SPDX-FileCopyrightText: 2020 Friedrich-Alexander University Erlangen-Nürnberg (FAU)
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package composer
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+
+	"github.com/osrgroup/product-model-toolkit/model"
+)
+
+// Composer represents a PHP Composer converter
+type Composer struct{}
+
+type composerDocComp struct {
+	Name        string
+	Version     string
+	Description string
+	License     []string
+}
+
+type composerDoc struct {
+	Installed []composerDocComp
+}
+
+// Convert converts a PHP Composer representation into a Product Model representation.
+func (Composer) Convert(doc []byte) (*model.Product, error) {
+	body := trimUTF8prefix(doc)
+	var result composerDoc
+	err := json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Product{
+		Name:       "unkown",
+		Components: extractComponents(&result),
+	}, nil
+}
+
+func extractComponents(input *composerDoc) []model.Component {
+	var comps []model.Component
+	for _, c := range input.Installed {
+		licenses := strings.Join(c.License, ", ")
+		comp := &model.Component{
+			Name:    c.Name,
+			Version: c.Version,
+			License: licenses,
+		}
+		comps = append(comps, *comp)
+	}
+
+	return comps
+}
+
+func trimUTF8prefix(doc []byte) []byte {
+	return bytes.TrimPrefix(doc, []byte("\xef\xbb\xbf"))
+}
---
+++ pkg/convert/composer/convert_test.go
@@ -0,0 +1,159 @@
+// SPDX-FileCopyrightText: 2020 Friedrich-Alexander University Erlangen-Nürnberg (FAU)
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package composer
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+var exampleDoc []byte
+
+// TestMain runs before all tests once
+func TestMain(m *testing.M) {
+	var err error
+	exampleDoc, err = readExampleDoc()
+	if err != nil {
+		log.Fatal("Unable to read example.json to start tests")
+		os.Exit(-1)
+	}
+	os.Exit(m.Run())
+}
+
+func readExampleDoc() ([]byte, error) {
+	jsonFile, err := os.Open("test/example.json")
+	if err != nil {
+		return nil, err
+	}
+
+	defer jsonFile.Close()
+
+	jsonBytes, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return jsonBytes, nil
+}
+
+func TestConvert(t *testing.T) {
+	c := &Composer{}
+	p, err := c.Convert(exampleDoc)
+
+	t.Run("not error", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Expeted convert func to not return an error, but got %v", err.Error())
+		}
+	})
+
+	t.Run("not nil", func(t *testing.T) {
+		if p == nil {
+			t.Error("Expected product to be not nil")
+		}
+	})
+
+	t.Run("product name", func(t *testing.T) {
+		if p.Name != "unkown" {
+			t.Errorf("Expected amount of components to be %v, but got %v", "unkown", p.Name)
+		}
+	})
+
+	t.Run("component amount", func(t *testing.T) {
+		if len(p.Components) != 197 {
+			t.Errorf("Expected amount of components to be %v, but got %v", 197, len(p.Components))
+		}
+	})
+	t.Run("component name", func(t *testing.T) {
+		first := p.Components[0].Name
+		firstExp := "bluespice/about"
+		last := p.Components[len(p.Components)-1].Name
+		lastExp := "zordius/lightncandy"
+
+		if first != firstExp {
+			t.Errorf("Expected name of first component to be %v, but got %v", firstExp, first)
+		}
+		if last != lastExp {
+			t.Errorf("Expected name of last component to be %v, but got %v", lastExp, last)
+		}
+	})
+
+	t.Run("component version", func(t *testing.T) {
+		first := p.Components[0].Version
+		firstExp := "dev-REL1_31"
+		last := p.Components[len(p.Components)-1].Version
+		lastExp := "v0.23"
+
+		if first != firstExp {
+			t.Errorf("Expected version of first component to be %v, but got %v", firstExp, first)
+		}
+		if last != lastExp {
+			t.Errorf("Expected version of last component to be %v, but got %v", lastExp, last)
+		}
+	})
+
+	t.Run("component license", func(t *testing.T) {
+		first := p.Components[0].License
+		firstExp := "GPL-3.0-only"
+		last := p.Components[len(p.Components)-1].License
+		lastExp := "MIT"
+
+		if first != firstExp {
+			t.Errorf("Expected license of first component to be %v, but got %v", firstExp, first)
+		}
+		if last != lastExp {
+			t.Errorf("Expected license of last component to be %v, but got %v", lastExp, last)
+		}
+	})
+}
+
+func TestConvert_Empty(t *testing.T) {
+	c := &Composer{}
+	p, err := c.Convert([]byte{})
+	if err == nil {
+		t.Error("Expeted returning an error for empty doc")
+	}
+	if p != nil {
+		t.Error("Expeted resulting product for empty doc to be nil")
+	}
+}
+
+func TestExtractComponents(t *testing.T) {
+	a := &composerDocComp{
+		Name:    "CompA",
+		Version: "0.4.2",
+		License: []string{"MIT", "Apache-2.0"},
+	}
+	b := &composerDocComp{
+		Name:    "CompB",
+		Version: "1.1.1",
+		License: []string{"GPL"},
+	}
+	input := &composerDoc{
+		Installed: []composerDocComp{*a, *b},
+	}
+
+	comps := extractComponents(input)
+	if comps == nil {
+		t.Errorf("Expected extractComponents() result not be nil")
+	}
+	if len(comps) != 2 {
+		t.Errorf("Expected to return %v components, but got %v", 2, len(comps))
+	}
+	if comps[0].Name != "CompA" {
+		t.Errorf("Expected name of first component to be %v, but got %v", "CompA", comps[0].Name)
+	}
+	if comps[0].Version != "0.4.2" {
+		t.Errorf("Expected version of first component to be %v, but got %v", "0.4.2", comps[0].Version)
+	}
+	if comps[0].License != "MIT, Apache-2.0" {
+		t.Errorf("Expected license of first component to be %v, but got %v", "MIT, Apache-2.0", comps[0].License)
+	}
+	if comps[1].License != "GPL" {
+		t.Errorf("Expected license of second component to be %v, but got %v", "GPL", comps[1].License)
+	}
+
+}
