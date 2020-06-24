29325e5336f892093f686ee2cf0032532018b26b
Andreas Bauer <dev@andreasbauer.org>
2020-06-24 15:24:34 +0200

    Extract trimUTF8prefix func to converter.go

--- pkg/convert/converter.go
+++ pkg/convert/converter.go
@@ -4,10 +4,18 @@
 
 package convert
 
-import "github.com/osrgroup/product-model-toolkit/model"
+import (
+	"bytes"
+
+	"github.com/osrgroup/product-model-toolkit/model"
+)
 
 // Converter is the interface all converter implementations need to fulfill.
 type Converter interface {
 	// Convert converts a doc to the product model representation.
 	Convert([]byte) (*model.Product, error)
+}
+
+func TrimUTF8prefix(doc []byte) []byte {
+	return bytes.TrimPrefix(doc, []byte("\xef\xbb\xbf"))
 }
