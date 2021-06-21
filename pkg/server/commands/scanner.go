35cfd27f87a6d7eefd9fc0cd200202b494d44a78
Reza Ghanbari <ghanbari.reza94@gmail.com>
2021-06-21 21:33:59 +0200

    Adding scanner files

--- pkg/server/commands/scanner.go
+++ pkg/server/commands/scanner.go
@@ -3,10 +3,107 @@ package commands
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"os/exec"
 
 	"github.com/osrgroup/product-model-toolkit/cnst"
 )
+
+// Scanner provides license scanner operations.
+type Scanner interface {
+	Exec(cfg Config)
+}
+
+// Tool struct define a scanner tool
+type Tool struct {
+	Name      string
+	Version   string
+	DockerImg string
+	Cmd       string
+	Results   []string
+}
+
+// Config represents a configuration for a tool to execute.
+type Config struct {
+	Tool
+	InDir     string
+	ResultDir string
+}
+
+// Available list all available scanner tools that can be used.
+var Available = [...]Tool{
+	Licensee,
+	Scancode,
+	Composer,
+	FileHasher,
+}
+
+// Default is the default scanner tools that shall be used if no particular tool is selected.
+var Default Tool = Licensee
+
+// Licensee represents the latest usable Licensee version
+var Licensee = Tool{
+	Name:      "Licensee",
+	Version:   "9.13.0",
+	DockerImg: "docker.pkg.github.com/osrgroup/product-model-toolkit/scanner-licensee:9.13.0",
+	Cmd:       `/bin/bash -c "licensee detect /input/ --json > /result/result.json"`,
+	Results:   []string{"result.json"},
+}
+
+// Scancode represents the latest usable Scancode version
+var Scancode = Tool{
+	Name:      "Scancode",
+	Version:   "3.1.1",
+	DockerImg: "docker.pkg.github.com/osrgroup/product-model-toolkit/scanner-scancode:3.1.1",
+	Cmd:       `/bin/bash -c "./scancode --spdx-tv /result/result.spdx /input"`,
+	Results:   []string{"result.spdx"},
+}
+
+
+// Composer represents a container which produces a Composer result artifact.
+var Composer = Tool{
+	Name:      "Composer",
+	Version:   "dummy",
+	DockerImg: "docker.pkg.github.com/osrgroup/product-model-toolkit/scanner-composer:dummy",
+	Cmd:       `/bin/sh -c "cp example.json result/example.json"`,
+	Results:   []string{"example.json"},
+}
+
+// FileHasher represents a container which produces a JSON file with hashes of all file of the input folder.
+var FileHasher = Tool{
+	Name:      "File-Hasher",
+	Version:   "latest",
+	DockerImg: "docker.pkg.github.com/osrgroup/product-model-toolkit/file-hasher:latest",
+	Cmd:       `/bin/sh -c "./fh -i /input -o /result/result.json"`,
+	Results:   []string{"result.json"},
+}
+
+var PhpScanner = Tool {
+	Name: "PHP-Scanner",
+	Version: "1.0.0",
+	DockerImg: "docker.pkg.github.com/osrgroup/product-model-toolkit/php-scanner:1.0.0",
+	Cmd: `/bin/sh -c  "phpScanner.php --sourcedir=<path/to/scanned/folder> --outputdir=<path/to/output/folder>"`, // TODO
+	Results: []string{"phpScanner.json"},
+
+}
+
+// FromStr returns a tool with the given name. If unable to find a tool with the given name it returns the default tool.
+func FromStr(name string) Tool {
+	search := strings.ToLower(name)
+	for _, t := range Available {
+		if strings.ToLower(t.Name) == search {
+			return t
+		}
+	}
+
+	return Default
+}
+
+// String return the name and the version of the tool.
+func (t *Tool) String() string {
+	return fmt.Sprintf("%s (%s)", t.Name, t.Version)
+}
+
 
 func RunScanner(name, source, output string) error {
 	// creating a new http client
