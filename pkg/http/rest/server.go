f30aab13345e94ddbbf37119bb0d62ebece6e7bb
Reza Ghanbari <ghanbari.reza94@gmail.com>
2021-06-28 22:04:22 +0200

    In the middle of adding ui

--- pkg/http/rest/server.go
+++ pkg/http/rest/server.go
@@ -31,7 +31,9 @@ func NewSrv(address string, srv services.Service) *Instance {
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
+	e.File("/", "public/index.html")
+	e.File("/products", "public/products.html")
+	e.File("/find-product-by-id", "public/findproduct.html")
 	// Routes
 	v1 := e.Group(cnst.APIGroup)
 	Handler(v1, srv)
