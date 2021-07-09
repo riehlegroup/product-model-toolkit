161c0c49a4f0a6547a24be89ab0e1c0293296299
Reza Ghanbari <ghanbari.reza94@gmail.com>
2021-07-09 23:34:24 +0200

    Delete docker/graphile directory

--- docker/graphile/clean-db-build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-# SPDX-FileCopyrightText: 2020 Friedrich-Alexander University Erlangen-Nürnberg (FAU)
-#
-# SPDX-License-Identifier: Apache-2.0
-
-docker-compose rm -f db
-docker-compose build db
