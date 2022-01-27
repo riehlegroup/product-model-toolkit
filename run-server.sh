d8e5459f7b98fa0403ec8f300a7727a79c2a4f66
Reza <ghanbari.reza94@gmail.com>
2022-01-27 18:22:53 +0100

    update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,5 @@
-docker-compose stop && docker-compose up -d --build && go build cmd/server/main.go && source env.txt && export $(cut -d= -f1 env.txt)
- &&  nohup ./main &
+docker-compose stop && docker-compose up -d --build 
+go build cmd/server/main.go 
+source env.txt 
+export $(cut -d= -f1 env.txt)
+nohup ./main &
