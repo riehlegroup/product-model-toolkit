d814f4a5fdd98d09706f29bf20a651cd171a8523
Reza <ghanbari.reza94@gmail.com>
2022-01-27 18:11:27 +0100

    Make the run-server file ready for production env

--- run-server.sh
+++ run-server.sh
@@ -1,1 +1,2 @@
-docker-compose stop && docker-compose up -d --build && nohup ./main &
+docker-compose stop && docker-compose up -d --build && go build cmd/server/main.go && source env.txt && export $(cut -d= -f1 env.txt)
+ &&  nohup ./main &
